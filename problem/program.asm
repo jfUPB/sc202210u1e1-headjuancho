6200989ab03142f039a0b0a5f8762e1481e5bc05
Juan Manuel Arciniegas <jnmnlarciniegas@gmail.com>
2022-02-10 22:48:01 -0500

    el programa ahora verifica si la letra f esta siendo presionada y define una posicion de la memoria como -1 para pintar

--- problem/program.asm
+++ problem/program.asm
@@ -1,6 +1,23 @@
 // JUAN MANUEL ARCINIEGAS
 // 000251686
 // juan.arciniegasm@upb.edu.co
+// F = 70
 (START)
+    @70
+    D=A
+    @KBD
+    D=D-M
+    @LEIDO
+    D;JEQ
+    @START
+    0;JMP
+(NEGRO)
+    @R0
+    M=-1
+    @FILL
+    0;JMP
+    @START
+    0;JMP
+(FILL)
     @START
     0;JMP
