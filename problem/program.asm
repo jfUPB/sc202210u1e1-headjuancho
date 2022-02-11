a0cbd5ac83c7fcf061c944276a97be66b36bdad2
Juan Manuel Arciniegas <jnmnlarciniegas@gmail.com>
2022-02-11 08:40:08 -0500

    SE FINALIZA EL EJERCICIO

--- problem/program.asm
+++ problem/program.asm
@@ -1,13 +1,23 @@
 // JUAN MANUEL ARCINIEGAS
 // 000251686
 // juan.arciniegasm@upb.edu.co
-// F = 70
+// F = 70 C=67
 (START)
+    @16384
+    D=A
+    @R1
+    M=D
     @70
     D=A
     @KBD
     D=D-M
-    @LEIDO
+    @NEGRO
+    D;JEQ
+    @67
+    D=A
+    @KBD
+    D=D-M
+    @BLANCO
     D;JEQ
     @START
     0;JMP
@@ -16,8 +26,26 @@
     M=-1
     @FILL
     0;JMP
-    @START
+   
+(BLANCO)
+    @R0
+    M=0
+    @FILL
     0;JMP
+
 (FILL)
+    @R1
+    D=M
+    @24575
+    D=D-A
     @START
+    D;JGT
+    @R0
+    D=M
+    @R1
+    A=M
+    M=D
+    @R1
+    M=M+1
+    @FILL
     0;JMP
